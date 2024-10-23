8175890b6a580b434cc60791ba48798ef93144aa
Manav2209 <manavpanchal2854@gmail.com>
2024-10-23 13:45:35 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,27 @@ import CodeEditor from "@/components/CodeEditor";
 import MenuBar from "@/components/MenuBar";
 import { fonts, themes } from "@/lib/options";
 import { codeAtom } from "@/store/codeAtom";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilValue } from "recoil";
 export default function Home() {
   const code = useRecoilValue(codeAtom);
   const theme = code.theme;
   const fontStyle = code.fontStyle;
 
+
+  // Track whether the component is mounted
+  const [mounted, setMounted] = useState(false);
+
   const editorRef = useRef<HTMLDivElement>(null);
+
+  // Ensure client-side only rendering
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Skip rendering until the component is mounted
+
+
   return (
     <main className="flex flex-col relative justify-center items-center h-[100vh] ">
       <link rel="stylesheet" href={themes[theme].theme} />
